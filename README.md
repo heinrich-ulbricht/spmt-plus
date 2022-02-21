c96e0874e743fbe2b91eb99e8be827edf5a73690
Heinrich Ulbricht <github@heinrich-ulbricht.de>
2022-02-21 12:12:57 +0100

    adding first version of patcher script and updating readme

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# spmt-plus
+# SPMT+
+
+Patches the [SharePoint Migration Tool (SPMT)](https://docs.microsoft.com/en-us/sharepointmigration/introducing-the-sharepoint-migration-tool) to support additional list and document library types.
+## Background
+
+The SharePoint Migration Tool only supportes certain SharePoint list templates.
+
+[Here's](https://docs.microsoft.com/en-us/sharepointmigration/sharepoint-migration-supported-list-templates) a list of supported template IDs that looks like this (excerpt):
+
+|**List template ID**|**Name**|**Description**|**Supported by SPMT**|
+|:-----|:-----|:-----|:-----|
+|-1|InvalidType|Not used. Value = -1|No|
+|0|NoListTemplate|unspecified list type. Value = 0|No|
+|100|GenericList|Custom list. Value = 100|Yes|
+|101|DocumentLibrary|Document library. Value = 101|Yes|
+||*Snip - lots of entries here*||
+|1220|HealthRules|Health Rules. Value = 1220|No|
+|1221|HealthReports|Health Reports. Value = 1221|No|
+|1230|DeveloperSiteDraftApps|Draft Apps library in Developer Site. Value = 1230|No|
+
+The *Supported by SPMT* column tells whether a type of list is supported or not. If you want to migrate one of those types you will be out of luck with SPMT.
+
+## Use case
+
+We had to migrate document libraries created by a SharePoint on-prem solution that were basically normal document libraries. They behaved like ID 101 (which is *DocumentLibrary*) but internally they've got an ID like 10015. This is not supported by SPMT.
+
+We ended up patching SPMT to force it to migrate those libraries nevertheless.
+
+## How SPMT normally rejects certain lists and libraries
+
+SPMT internally has several checks in place to ensure it only migrates supported lists and libraries.
+
+SPMT uses a whitelist of supported list template types. It also excludes lists based on their title (like "Style Library") and on metadata like "is a catalog". There are also checks for mismatches between the source list type and the destination list type (if it already exists). Certain mappings are supported, most not. Same goes for content types. Some special content types are force-mapped to more generic content types, but most aren't.
+
+## How does the patching script work
+
+The patcher script disables the checks that SPMT does that prevented our lists from being migrated.
+
+It uses the Harmony library (https://github.com/pardeike/Harmony) to modify the SPMT PowerShell cmdlets in memory. Harmony is able to add additional logic before and after methods of other .NET applications (which SPMT is). See the [Harmony documentation](https://harmony.pardeike.net/) for details on how this works.
+
+The following checks are modified:
+
+- additional types are added to the list of supported list template types (you must add your type here)
+- disabling the check for unsupported list names
+- disabling the check for unsupported catalog lists
+- disabling the list template type mismatch check by setting the source list template type to the target list template type (this might not work for your scenario)
+
+There are some side effects, e.g. the check for audience targeted lists is also disabled rendering the option of SPMT to ignore those lists impossible. Other side effects might occur.
+
+## How to use this script
+
+- install the *SharePoint Migration Tool* first - it comes with PowerShell cmdlets you will use to migrate (sorry, no UI)
+  - note: those cmdlets will be patched
+- download Harmony and extract the *0Harmony.dll* from the [zip file](https://github.com/pardeike/Harmony/releases/tag/v2.2.0.0) (I successfully used the net4.8 variant):
+    - put 0Harmony.dll in the same folder as the script _or_ in the current directory; the script looks in both places
+- run the script
+    - the console should be free of red error messages and only contain green or white status messages
+- include this script into your migration script to patch SPMT before starting the migration with *Start-SPMTMigration*
+- the script can be safely run multiple times
+
+A successful script run will look like this:
+
+![Console output of a successful script run](images/successful-run.png)
+
+
+This script has been tested with:
+- Visual Studio Code with PowerShell extension
+    - note: VS Code sometimes liked to crash after restarting the PowerShell session - just restart VS Code in this case
+    - note: PowerShell ISE should also work but added console output from the patched methods might be missing
+- SPMT Prerelease 3.5.123.2
+- PowerShell 5.1
+- Harmony 2.2.0.0, .NET 4.8 DLL
+- SharePoint 2013 Server to SharePoint Online migration
+
+## Notes
+
+Add additional list template types you need to the `GetSupportedListTemplatesPostfix` method. Other checks for title etc. are already completely disabled.
+
+Since I couldn't get the SPMT to produce a decent log file I also patched the logging methods and write the log output to the `spmtplus.log` file that will be created in the current directory.
+
+Some log messages will also be written to the console, check the methods like `LogDebugPrefix` to modify this as you wish.
+
+You can see and modify all of this additional functionality in the patch script!
+## More list template information
+- the list of SharePoint list template types supported by SPMT can be seen here: https://docs.microsoft.com/en-us/sharepointmigration/sharepoint-migration-supported-list-templates
+- a list of SharePoint list template types can be seen here: https://docs.microsoft.com/en-us/dotnet/api/microsoft.sharepoint.splisttemplatetype?view=sharepoint-server (it does not contain all)
+- other monikers for "list template type" are BaseTemplate, BaseTemplateId, ListTemplate and ListTemplateType
+
+## Disclaimer
+
+Use at your own risk. Migrating all lists might now be possible, but does not always make sense. List functionality that is backed by code will not run on SharePoint Online. Certain content types may behave differently in SPO than on-prem. Side effects might occur that surface outside the tested scope.
+
+I recommend using the patched SPMT to only migrate those lists and libraries that require patching and to carefully check the results. Otherwise you should use the unpatched version.
+
+## Note to Microsoft
+
+Please open source the SharePoint Migration Tool as this would make things easier. Thank you.
---
+++ SpmtPatcher.ps1
@@ -0,0 +1,324 @@
+<#
+    Notes: 
+    - have SPMT intalled
+    - put 0Harmony.dll in the script directory
+    - run the script
+    - restart PowerShell process after modifications
+    - no red error messages = success
+#>
+
+function LoadSpmtPowerShellModule() {
+    if (Get-Module Microsoft.SharePoint.MigrationTool.PowerShell) {
+        Write-Host "SPMT PowerShell module is loaded, good"
+        return
+    }
+    
+    $currentLocation = Get-Location
+    $spmtModulePath = "$($env:UserProfile)\Documents\WindowsPowerShell\Modules\Microsoft.SharePoint.MigrationTool.PowerShell"
+    Write-Host "Importing SPMT PowerShell module from '$spmtModulePath'"
+    if (-not (Get-Item $spmtModulePath -ErrorAction SilentlyContinue).Exists) {
+        throw "SharePoint Migration Toolkit PowerShell Module expected but not found in '$spmtModulePath'"
+  
+    }
+    Set-Location $spmtModulePath
+    $null = Import-Module "Microsoft.SharePoint.MigrationTool.PowerShell"
+    Set-Location $currentLocation
+  
+    if (Get-Module Microsoft.SharePoint.MigrationTool.PowerShell) {
+        Write-Host "SPMT PowerShell module loaded" -ForegroundColor Green
+    }
+    else {
+        throw "SPMT PowerShell module not loaded, cannot continue"
+    }
+}
+  
+function LoadHarmony() {
+    $harmonyType = ([System.Management.Automation.PSTypeName]'HarmonyLib.Harmony').Type
+    if ($harmonyType) {
+        Write-Host "Harmony is already loaded, good"
+        return
+    }
+  
+    $harmonyDllPath = "0Harmony.dll"
+    if (-not (Get-Item $harmonyDllPath -ErrorAction SilentlyContinue).Exists) {
+        if ($PSScriptRoot) {
+            $harmonyDllPath = "$PSScriptRoot\0Harmony.dll"
+            if (-not (Get-Item $harmonyDllPath -ErrorAction SilentlyContinue).Exists) {
+                throw "Did not find Harmony DLL at path '$harmonyDllPath'"
+            }
+        }
+        else {
+            Write-Host "Cannot search in script root since you are only running a selection of the script file" -ForegroundColor Yellow
+            throw "Did not find Harmony DLL at path '$harmonyDllPath'"
+        }
+    }
+    Write-Host "Found Harmony DLL at path '$harmonyDllPath'" -ForegroundColor Green
+  
+    $null = Add-Type -LiteralPath $harmonyDllPath
+    $harmonyType = ([System.Management.Automation.PSTypeName]'HarmonyLib.Harmony').Type
+    if ($harmonyType) {
+        $harmonyTargetFramework = [HarmonyLib.Harmony].Assembly.CustomAttributes |
+        Where-Object { $_.AttributeType.Name -eq "TargetFrameworkAttribute" } | 
+        Select-Object -ExpandProperty ConstructorArguments | 
+        Select-Object -ExpandProperty value
+  
+        Write-Host "Harmony is loaded; version: $([HarmonyLib.Harmony].Assembly.GetName().Version.ToString()); ImageRuntimeVersion: $([HarmonyLib.Harmony].Assembly.ImageRuntimeVersion); TargetFramework: $($harmonyTargetFramework)" -ForegroundColor Green
+    }
+    else {
+        throw "Harmony not loaded"
+    }
+}
+  
+function CreateCustomTypeForHarmony() {
+    $Version = 1
+  
+    $spmtModificationsType = ([System.Management.Automation.PSTypeName]'SpmtModifications').Type
+    # check if version matches or if we got an old type in memory
+    if ($spmtModificationsType) {
+        if ([SpmtModifications]::Version -ne $Version) {
+            throw "Detected old SpmtModifications type in memory. This can happen after code modifications. Please restart the PowerShell process and run the script again."
+        }
+        Write-Host "Found existing custom SPMT modification type, good"
+        return
+    }
+  
+    $Source = @"
+    public class SpmtModifications
+    {
+      // note: __result contains the result value of the called method; in a Harmony postfix method we get the chance to modify the result value
+      // see here for how this works: https://harmony.pardeike.net/articles/intro.html#how-harmony-works
+  
+      public static long Version = 1;
+      public static void ValidateListMetaInfoPostfix(Microsoft.SharePoint.MigrationTool.MigrationLib.SharePoint.IList list, string listCultureInvariantTitle, ref Microsoft.SharePoint.MigrationTool.MigrationLib.Schema.SchemaMigrationMessage __result)
+      {
+          System.Console.WriteLine("Original ValidateListMetaInfo result for '" + listCultureInvariantTitle + "': " + __result.ToString());
+          Log("[HEU] Original ValidateListMetaInfo result for '" + listCultureInvariantTitle + "': " + __result.ToString());
+  
+          // sample of explicitly handling one library by title
+          if (listCultureInvariantTitle == "Style Library")
+          {
+            System.Console.WriteLine("Changing result for Style Library");
+            Log("[HEU] Changing result for Style Library");
+            __result = Microsoft.SharePoint.MigrationTool.MigrationLib.Schema.SchemaMigrationMessage.None;
+          }
+    
+          // or just allow everything...
+          __result = Microsoft.SharePoint.MigrationTool.MigrationLib.Schema.SchemaMigrationMessage.None;
+      }
+    
+      public static void IsFilteredListPostfix(Microsoft.SharePoint.MigrationTool.MigrationLib.SharePoint.IList docLib, ref bool __result)
+      {
+          System.Console.WriteLine("Original IsFilteredList result for: '" + docLib.RootFolder.ServerRelativeUrl + "' (BaseTemplate " + docLib.BaseTemplate.ToString() + "): " + __result.ToString());
+          Log("[HEU] Original IsFilteredList result for: '" + docLib.RootFolder.ServerRelativeUrl + "' (BaseTemplate " + docLib.BaseTemplate.ToString() + "): " + __result.ToString());
+  
+          // allow everything, filter nothing...
+          __result = false;
+      }
+  
+      public static bool CheckListTemplateMatchPrefix(Microsoft.SharePoint.MigrationTool.MigrationLib.Common.ListMetaInfo sourceList, Microsoft.SharePoint.MigrationTool.MigrationLib.Common.ListMetaInfo targetList, Microsoft.SharePoint.MigrationTool.MigrationLib.Common.PackageSourceType SourceType)
+      {
+          System.Console.WriteLine("Skipping schema mismatch check");
+          Log("[HEU] Skipping schema mismatch check");
+          if (targetList.ListTemplateType != sourceList.ListTemplateType)
+          {
+            System.Console.WriteLine("Using the target document library type {0} instead of the default {1} for list {2}", (object) targetList.ListTemplateType, (object) sourceList.ListTemplateType, (object) targetList.Url);
+            Log("[HEU] Using the target document library type {0} instead of the default {1} for list {2}", (object) targetList.ListTemplateType, (object) sourceList.ListTemplateType, (object) targetList.Url);
+            sourceList.ListTemplateType = targetList.ListTemplateType;
+          }
+          
+          return false;
+      }
+  
+      public static void PostUpdateListSchemaPrefix(Microsoft.SharePoint.MigrationTool.MigrationLib.Common.ListMetaInfo sourceList, Microsoft.SharePoint.MigrationTool.MigrationLib.Common.ListMetaInfo targetList)
+      {
+          Log("[HEU] PostUpdateListSchemaPrefix");
+          Microsoft.SharePoint.MigrationTool.MigrationLib.Assessment.SchemaScanSpAbstract.ContentTypeManageDisable.Add(Microsoft.SharePoint.Client.ListTemplateType.DocumentLibrary);
+      }
+  
+      public static void PostUpdateListSchemaPostfix(Microsoft.SharePoint.MigrationTool.MigrationLib.Common.ListMetaInfo sourceList, Microsoft.SharePoint.MigrationTool.MigrationLib.Common.ListMetaInfo targetList)
+      {
+        Log("[HEU] PostUpdateListSchemaPostfix");
+        Microsoft.SharePoint.MigrationTool.MigrationLib.Assessment.SchemaScanSpAbstract.ContentTypeManageDisable.Remove(Microsoft.SharePoint.Client.ListTemplateType.DocumentLibrary);
+      }
+  
+      public static void Log(string errorMessage = "", params object[] messageArgs)
+      {
+        bool retry;
+        // solve concurrency issues by retrying; not pretty, but works
+        do
+        {
+          try
+          {
+            System.IO.File.AppendAllText("spmtplus.log", string.Format(errorMessage+System.Environment.NewLine, messageArgs));
+            retry = false;
+          } catch
+          {
+            retry = true;
+          }
+        } while (retry);
+      }
+  
+      public static void LogDebugPrefix(string errorMessage = "", params object[] messageArgs)
+      {
+        // System.Console.WriteLine("[DBG] " + errorMessage, messageArgs);
+        Log("[DBG] " + errorMessage, messageArgs);
+      }
+  
+      public static void LogInformationPrefix(string errorMessage = "", params object[] messageArgs)
+      {
+        // System.Console.WriteLine("[INF] " + errorMessage, messageArgs);
+        Log("[INF] " + errorMessage, messageArgs);
+      }
+  
+      public static void LogWarningPrefix(string errorMessage = "", params object[] messageArgs)
+      {
+        System.Console.WriteLine("[WRN] " + errorMessage, messageArgs);
+        Log("[WRN] " + errorMessage, messageArgs);
+      }
+  
+      public static void LogErrorPrefix(string errorMessage = "", params object[] messageArgs)
+      {
+        System.Console.WriteLine("[ERR] " + errorMessage, messageArgs);
+        Log("[ERR] " + errorMessage, messageArgs);
+      }
+  
+      public static void LogVerbosePrefix(string errorMessage = "", params object[] messageArgs)
+      {
+        // System.Console.WriteLine("[VER] " + errorMessage, messageArgs);
+        Log("[VER] " + errorMessage, messageArgs);
+      }
+  
+      public static void LogExceptionPrefix(System.Exception e, string errorMessage = "", params object[] messageArgs)
+      {
+        System.Console.WriteLine("[EXC] " + errorMessage + " (" + e.ToString() + ")", messageArgs);
+        Log("[EXC] " + errorMessage + " (" + e.ToString() + ")", messageArgs);
+      }
+  
+      public static void GetSupportedListTemplatesPostfix(ref System.Collections.Generic.HashSet<Microsoft.SharePoint.Client.ListTemplateType> __result)
+      {
+          // note: this is the original SchemaScanSpAbstract.SupportedListTemplates plus added template types we want SPMT to support
+          __result = new System.Collections.Generic.HashSet<Microsoft.SharePoint.Client.ListTemplateType>()
+          {
+            Microsoft.SharePoint.Client.ListTemplateType.DocumentLibrary,
+            Microsoft.SharePoint.Client.ListTemplateType.MySiteDocumentLibrary,
+            Microsoft.SharePoint.Client.ListTemplateType.PictureLibrary,
+            Microsoft.SharePoint.Client.ListTemplateType.Announcements,
+            Microsoft.SharePoint.Client.ListTemplateType.Contacts,
+            Microsoft.SharePoint.Client.ListTemplateType.GanttTasks,
+            Microsoft.SharePoint.Client.ListTemplateType.GenericList,
+            Microsoft.SharePoint.Client.ListTemplateType.DiscussionBoard,
+            Microsoft.SharePoint.Client.ListTemplateType.Events,
+            Microsoft.SharePoint.Client.ListTemplateType.IssueTracking,
+            Microsoft.SharePoint.Client.ListTemplateType.Links,
+            Microsoft.SharePoint.Client.ListTemplateType.Tasks,
+            Microsoft.SharePoint.Client.ListTemplateType.Survey,
+            Microsoft.SharePoint.Client.ListTemplateType.TasksWithTimelineAndHierarchy,
+            Microsoft.SharePoint.Client.ListTemplateType.XMLForm,
+            Microsoft.SharePoint.Client.ListTemplateType.Posts,
+            Microsoft.SharePoint.Client.ListTemplateType.Comments,
+            Microsoft.SharePoint.Client.ListTemplateType.Categories,
+            Microsoft.SharePoint.Client.ListTemplateType.CustomGrid,
+            Microsoft.SharePoint.Client.ListTemplateType.WebPageLibrary,
+            Microsoft.SharePoint.Client.ListTemplateType.PromotedLinks,
+            (Microsoft.SharePoint.Client.ListTemplateType) 500,
+            (Microsoft.SharePoint.Client.ListTemplateType) 851,
+            (Microsoft.SharePoint.Client.ListTemplateType) 880,
+            (Microsoft.SharePoint.Client.ListTemplateType) 1302,
+            (Microsoft.SharePoint.Client.ListTemplateType) 10015,
+            (Microsoft.SharePoint.Client.ListTemplateType) 10017,
+            (Microsoft.SharePoint.Client.ListTemplateType) 10018,
+            (Microsoft.SharePoint.Client.ListTemplateType) 10019,
+            (Microsoft.SharePoint.Client.ListTemplateType) 5001, // added: Nintex
+            (Microsoft.SharePoint.Client.ListTemplateType) 550, // added: Social
+            (Microsoft.SharePoint.Client.ListTemplateType) 544 // added: MicroFeed
+            // [EXTENSION POINT] add the types you need; don't forget the comma in the previous line
+          }; 
+      } 
+    }
+"@
+  
+    $spmtModule = Get-Module "Microsoft.SharePoint.MigrationTool.PowerShell"
+    $spmtModulePath = (Get-Item $spmtModule.Path).Directory.FullName
+  
+    Write-Host "Loading types from SPMT module path '$spmtModulePath'"
+    [void][reflection.assembly]::LoadFrom("$spmtModulePath\microsoft.sharepoint.migrationtool.migrationlib.dll")
+    $null = Add-Type -TypeDefinition $Source -ReferencedAssemblies "mscorlib", "$spmtModulePath\microsoft.sharepoint.client.dll", "$spmtModulePath\microsoft.sharepoint.migrationtool.migrationlib.dll", "System.Console", "$spmtModulePath\microsoft.sharepoint.client.runtime.dll", "System.Core", "System.Runtime" -ErrorAction Continue
+  
+    $spmtModificationsType = ([System.Management.Automation.PSTypeName]'SpmtModifications').Type
+    if (-not $spmtModificationsType) {
+        throw "Could not create SpmtModifications type"
+    }
+  
+    Write-Host "Created custom SPMT modification type" -ForegroundColor Green
+}
+  
+function PatchSpmt() {
+    if ($Global:patched) {
+        Write-Host "SPMT methods are already patched, good"
+        return
+    }
+  
+    $harmony = New-Object HarmonyLib.Harmony -ArgumentList "com.heinrich.spmtredirects"
+    $validateListMetaInfoOrig = [Microsoft.SharePoint.MigrationTool.MigrationLib.Assessment.SchemaScanSpAbstract].GetMethod("ValidateListMetaInfo", [System.Reflection.BindingFlags]::Instance + [System.Reflection.BindingFlags]::NonPublic)
+    $validateListMetaInfoPostfix = [SpmtModifications].GetMethod("ValidateListMetaInfoPostfix", [System.Reflection.BindingFlags]::Static + [System.Reflection.BindingFlags]::Public)  
+    $patchResult = $harmony.Patch($validateListMetaInfoOrig, $null, (New-Object HarmonyLib.HarmonyMethod -ArgumentList $validateListMetaInfoPostfix))
+    if (-not $patchResult) {
+        throw "Could not patch ValidateListMetaInfo"
+    }
+    else {
+        Write-Host "Patched ValidateListMetaInfo" -ForegroundColor Green
+    }
+    
+    $isFilteredListOrig = [Microsoft.SharePoint.MigrationTool.MigrationLib.SharePoint.SPODocumentAcquirer].GetMethod("IsFilteredList", [System.Reflection.BindingFlags]::Instance + [System.Reflection.BindingFlags]::NonPublic)
+    $isFilteredListPostfix = [SpmtModifications].GetMethod("IsFilteredListPostfix", [System.Reflection.BindingFlags]::Static + [System.Reflection.BindingFlags]::Public)
+    $patchResult = $harmony.Patch($isFilteredListOrig, $null, (New-Object HarmonyLib.HarmonyMethod -ArgumentList $isFilteredListPostfix))
+    if (-not $patchResult) {
+        throw "Could not patch IsFilteredList"
+    }
+    else {
+        Write-Host "Patched IsFilteredList" -ForegroundColor Green
+    }
+  
+    $checkListTemplateMatchOrig = [Microsoft.SharePoint.MigrationTool.MigrationLib.Schema.SchemaUtilities].GetMethod("CheckListTemplateMatch", [System.Reflection.BindingFlags]::Static + [System.Reflection.BindingFlags]::Public)
+    $checkListTemplateMatchPrefix = [SpmtModifications].GetMethod("CheckListTemplateMatchPrefix", [System.Reflection.BindingFlags]::Static + [System.Reflection.BindingFlags]::Public)
+    $patchResult = $harmony.Patch($checkListTemplateMatchOrig, (New-Object HarmonyLib.HarmonyMethod -ArgumentList $checkListTemplateMatchPrefix), $null)
+    if (-not $patchResult) {
+        throw "Could not patch CheckListTemplateMatch"
+    }
+    else {
+        Write-Host "Patched CheckListTemplateMatch" -ForegroundColor Green
+    }
+  
+    # remove types you don't want to log
+    $logTypes = "Information", "Error", "Warning", "Exception", "Debug", "Verbose"
+    foreach ($logType in $logTypes) {
+        $logDebugOrig = [Microsoft.SharePoint.MigrationTool.MigrationLib.Common.MigObjectAbstract].GetMethod("Log$($logType)", [System.Reflection.BindingFlags]::Instance + [System.Reflection.BindingFlags]::Public)
+        $logDebugPrefix = [SpmtModifications].GetMethod("Log$($logType)Prefix", [System.Reflection.BindingFlags]::Static + [System.Reflection.BindingFlags]::Public)
+        $patchResult = $harmony.Patch($logDebugOrig, (New-Object HarmonyLib.HarmonyMethod -ArgumentList $logDebugPrefix), $null)
+        if (-not $patchResult) {
+            throw "Could not patch Log$($logType)"
+        }
+        else {
+            Write-Host "Patched Log$($logType)" -ForegroundColor Green
+        }
+    }
+  
+    $prop = [Microsoft.SharePoint.MigrationTool.MigrationLib.Assessment.SchemaScanSpAbstract].GetProperty("SupportedListTemplates", [System.Reflection.BindingFlags]::Static + [System.Reflection.BindingFlags]::Public)
+    $getter = $prop.GetAccessors()
+    $getSupportedListTemplatesOrig = $getter[0]
+    $getSupportedListTemplatesPostfix = [SpmtModifications].GetMethod("GetSupportedListTemplatesPostfix", [System.Reflection.BindingFlags]::Static + [System.Reflection.BindingFlags]::Public)
+    $patchResult = $harmony.Patch($getSupportedListTemplatesOrig, $null, (New-Object HarmonyLib.HarmonyMethod -ArgumentList $getSupportedListTemplatesPostfix))  
+    if (-not $patchResult) {
+        throw "Could not patch GetSupportedListTemplates"
+    }
+    else {
+        Write-Host "Patched GetSupportedListTemplates" -ForegroundColor Green
+    }
+  
+    $Global:patched = $true
+}
+  
+LoadSpmtPowerShellModule
+LoadHarmony
+CreateCustomTypeForHarmony
+PatchSpmt
